46659ee60fed995951fd2e7fd57714ecf00662aa
RaphaelIT7 <raphael.i.t@outlook.de>
2024-07-17 21:54:19 +0200

    Maybe show the store id?
    What is going on there.

--- source/modules/filesystem.cpp
+++ source/modules/filesystem.cpp
@@ -583,6 +583,7 @@ bool hook_CBaseFileSystem_FindNextFileHelper(CBaseFileSystem* filesystem, CBaseF
 	Msg("Wildcard: %s\n", data->wildCardString.Base());
 	Msg("Dir: %s\n", data->findData.cBaseDir);
 	Msg("Name: %s\n", data->findData.cFileName);
+	Msg("ID: %i\n", data->m_CurrentStoreID);
 	CSearchPath* path = func_CBaseFileSystem_FindSearchPathByStoreId(filesystem, data->m_CurrentStoreID);
 	if (path)
 		Msg("Path dir: %s\n", path->GetPathString());
@@ -603,6 +604,7 @@ bool hook_CBaseFileSystem_FindNextFileHelper(CBaseFileSystem* filesystem, CBaseF
 
 	Msg("New Dir: %s\n", data->findData.cBaseDir);
 	Msg("New Name: %s\n", data->findData.cFileName);
+	Msg("New ID: %i\n", data->m_CurrentStoreID);
 	CSearchPath* path3 = func_CBaseFileSystem_FindSearchPathByStoreId(filesystem, data->m_CurrentStoreID);
 	if (path3)
 		Msg("Path dir: %s\n", path3->GetPathString());
