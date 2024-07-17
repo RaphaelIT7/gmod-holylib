174fa170918433b7cd51da3d125511825c5f6188
RaphaelIT7 <raphael.i.t@outlook.de>
2024-07-17 12:30:22 +0200

    Start sourcetv

--- source/modules/sourcetv.cpp
+++ source/modules/sourcetv.cpp
@@ -1,6 +1,7 @@
 #include "module.h"
 #include <GarrysMod/Lua/Interface.h>
 #include "lua.h"
+#include "sourcesdk/baseserver.h"
 
 class CSourceTVLibModule : public IModule
 {
@@ -14,10 +15,18 @@ public:
 	virtual const char* Name() { return "sourcetv"; };
 };
 
-ConVar sourcetv_allownetworkin("holylib_sourcetv_allownetworkin", "0", 0, "Allows HLTV Clients to send net messages to the server.");
+ConVar sourcetv_allownetworking("holylib_sourcetv_allownetworking", "0", 0, "Allows HLTV Clients to send net messages to the server.");
 
 CSourceTVLibModule g_pSourceTVLibModule;
 IModule* pSourceTVLibModule = &g_pSourceTVLibModule;
+
+CBaseServer** hltv;
+LUA_FUNCTION_STATIC(sourcetv_IsActive)
+{
+	LUA->PushBool((*hltv)->IsActive());
+	
+	return 1;
+}
 
 void CSourceTVLibModule::Init(CreateInterfaceFn* appfn, CreateInterfaceFn* gamefn)
 {
@@ -27,18 +36,27 @@ void CSourceTVLibModule::LuaInit(bool bServerInit)
 {
 	if (!bServerInit)
 	{
-
+		Util::StartTable();
+			Util::AddFunc(sourcetv_IsActive, "IsActive");
+		Util::FinishTable("sourcetv");
 	}
 }
 
 void CSourceTVLibModule::LuaShutdown()
 {
+	g_Lua->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+		g_Lua->PushNil();
+		g_Lua->SetField(-2, "sourcetv");
+	g_Lua->Pop(1);
 }
 
 void CSourceTVLibModule::InitDetour(bool bPreServer)
 {
 	if ( bPreServer ) { return; }
 
+	SourceSDK::FactoryLoader engine_loader("engine_srv");
+	hltv = ResolveSymbol<CBaseServer*>(engine_loader, Symbols::hltvSym);
+	Detour::CheckValue("get class", "hltv", hltv != NULL);
 }
 
 void CSourceTVLibModule::Think(bool simulating)
