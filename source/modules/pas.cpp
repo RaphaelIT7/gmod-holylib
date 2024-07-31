41da11cbd3db63c11275888b4633aa1ef8390d8e
RaphaelIT7 <raphael.i.t@outlook.de>
2024-07-31 17:47:59 +0200

    Fixing the compile

--- source/modules/pas.cpp
+++ source/modules/pas.cpp
@@ -23,6 +23,7 @@ IModule* pPASModule = &g_pPASModule;
 
 // NOTE: We should move the CM stuff into a seperate file :|
 CCollisionBSPData* gBSPData;
+CCollisionBSPData g_BSPData; // The compiler needs it :<
 int CM_PointLeafnum_r(CCollisionBSPData *pBSPData, const Vector& p, int num)
 {
 	float		d;
@@ -160,30 +161,32 @@ const byte *CM_Vis( byte *dest, int destlen, int cluster, int visType )
 LUA_FUNCTION_STATIC(pas_TestPAS) // This is based off SV_DetermineMulticastRecipients
 {
 	Vector* orig;
-	if (LUA->IsType(1, GarrysMod::Lua::Type::Vector))
-	{
+	LUA->CheckType(1, GarrysMod::Lua::Type::Vector);
+	//if (LUA->IsType(1, GarrysMod::Lua::Type::Vector))
+	//{
 		orig = Get_Vector(1);
-	} else {
+	/*} else {
 		LUA->CheckType(1, GarrysMod::Lua::Type::Entity);
 		CBaseEntity* ent = Util::Get_Entity(1, false);
 		if (!ent)
 			LUA->ArgError(1, "Tried to use a NULL entity");
 
-		orig = (Vector*)&ent->GetAbsOrigin();
-	}
+		orig = (Vector*)&ent->GetAbsOrigin(); // ToDo: This currently breaks the compile.
+	}*/
 
 	Vector* hearPos;
-	if (LUA->IsType(2, GarrysMod::Lua::Type::Vector))
-	{
+	LUA->CheckType(2, GarrysMod::Lua::Type::Vector);
+	//if (LUA->IsType(2, GarrysMod::Lua::Type::Vector))
+	//{
 		hearPos = Get_Vector(2);
-	} else {
+	/*} else {
 		LUA->CheckType(2, GarrysMod::Lua::Type::Entity);
 		CBaseEntity* ent = Util::Get_Entity(2, false);
 		if (!ent)
 			LUA->ArgError(2, "Tried to use a NULL entity");
 
 		hearPos = (Vector*)&ent->GetAbsOrigin();
-	}
+	}*/
 
 	int cluster = CM_LeafCluster(CM_PointLeafnum(*orig));
 	byte pvs[MAX_MAP_LEAFS/8];
