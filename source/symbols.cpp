0ec33f1a678798fe15e5c3302f8782ca3364bb79
RaphaelIT7 <raphael.i.t@outlook.de>
2024-08-09 01:45:47 +0200

    Try a symbol that hopefully shouldn't break so often

--- source/symbols.cpp
+++ source/symbols.cpp
@@ -6,7 +6,7 @@ namespace Symbols
 	//---------------------------------------------------------------------------------
 	const std::vector<Symbol> InitLuaClassesSym = {
 		Symbol::FromName("_Z14InitLuaClassesP13ILuaInterface"),
-		Symbol::FromSignature("\x48\x8B\x05\x59\x8B\xF9\x00"), // 48 8B 05 59 8B F9 00
+		Symbol::FromSignature("\x48\x8B\x05****\x48\x85\xC0**\x8B\x50\x10\x85\xD2**\x55\x48\x89\xE5\x53\x31\xDB******\x48\x8B*\x48\x8B\x3C\xD8\xE8\xD4"), // 48 8B 05 ?? ?? ?? ?? 48 85 C0 ?? ?? 8B 50 10 85 D2 ?? ?? 55 48 89 E5 53 31 DB ?? ?? ?? ?? ?? ?? 48 8B ?? 48 8B 3C D8 E8 D4
 	};
 
 	const std::vector<Symbol> CLuaInterface_ShutdownSym = {
