276c014bb96ae44d77de3b4bd1b52f0ef1103e9d
RaphaelIT7 <raphael.i.t@outlook.de>
2024-09-03 21:21:57 +0200

    threadpoolfix: add a fix for 64x threadpool

--- source/modules/threadpoolfix.cpp
+++ source/modules/threadpoolfix.cpp
@@ -4,15 +4,18 @@
 #include "detours.h"
 #include "util.h"
 #include "lua.h"
-#include <vstdlib/jobthread.h>
+#define DEDICATED
+#include "vstdlib/jobthread.h"
 
 class CThreadPoolFixModule : public IModule
 {
 public:
+	virtual void Init(CreateInterfaceFn* appfn, CreateInterfaceFn* gamefn) OVERRIDE;
 	virtual void InitDetour(bool bPreServer) OVERRIDE;
 	virtual const char* Name() { return "threadpoolfix"; };
-	virtual int Compatibility() { return LINUX32; }; 
+	virtual int Compatibility() { return LINUX32 | LINUX64; }; 
 	// NOTE: This is only an issue specific to Linux 32x DS so this doesn't need to support anything else.
+	// NOTE2: 64x has a different issue: https://github.com/Facepunch/garrysmod-issues/issues/5310
 };
 
 static CThreadPoolFixModule g_pThreadPoolFixModule;
@@ -27,14 +30,31 @@ int hook_CThreadPool_ExecuteToPriority(IThreadPool* pool, void* idx, void* idx2)
 	return detour_CThreadPool_ExecuteToPriority.GetTrampoline<Symbols::CThreadPool_ExecuteToPriority>()(pool, idx, idx2);
 }
 
+void CThreadPoolFixModule::Init(CreateInterfaceFn* appfn, CreateInterfaceFn* gamefn)
+{
+#ifdef ARCHITECTURE_X86_64
+	if (g_pThreadPool->NumThreads() > 0)
+	{
+		DevMsg("holylib: Threadpool is already running? Skipping our fix.");
+		return;
+	}
+
+	ThreadPoolStartParams_t startParams;
+	startParams.bEnableOnLinuxDedicatedServer = true;
+	g_pThreadPool->Start(startParams);
+#endif
+}
+
 void CThreadPoolFixModule::InitDetour(bool bPreServer)
 {
 	if ( !bPreServer ) { return; }
 
+#ifndef ARCHITECTURE_X86_64
 	SourceSDK::ModuleLoader libvstdlib_loader("libvstdlib_srv");
 	Detour::Create(
 		&detour_CThreadPool_ExecuteToPriority, "CThreadPool::ExecuteToPriority",
 		libvstdlib_loader.GetModule(), Symbols::CThreadPool_ExecuteToPrioritySym,
 		(void*)hook_CThreadPool_ExecuteToPriority, m_pID
 	);
+#endif
 }
