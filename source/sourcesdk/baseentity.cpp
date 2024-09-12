5f094a9dd1eda5a82da3f25c7408303af390b3ba
RaphaelIT7 <raphael.i.t@outlook.de>
2024-09-12 14:07:50 +0200

    sdk: make it happy

--- source/sourcesdk/baseentity.cpp
+++ source/sourcesdk/baseentity.cpp
@@ -9,3 +9,39 @@ void CBaseEntity::CalcAbsolutePosition(void)
 {
 	// ToDo: Load Gmod's version and call it here. For not it'll be empty
 }
+
+CBaseEntity* CGlobalEntityList::NextEnt(CBaseEntity* pCurrentEnt)
+{
+	if (!pCurrentEnt)
+	{
+		const CEntInfo* pInfo = FirstEntInfo();
+		if (!pInfo)
+			return NULL;
+
+		return (CBaseEntity*)pInfo->m_pEntity;
+	}
+
+	// Run through the list until we get a CBaseEntity.
+	const CEntInfo* pList = GetEntInfoPtr(pCurrentEnt->GetRefEHandle());
+	if (pList)
+		pList = NextEntInfo(pList);
+
+	while (pList)
+	{
+#if 0
+		if (pList->m_pEntity)
+		{
+			IServerUnknown* pUnk = static_cast<IServerUnknown*>(const_cast<IHandleEntity*>(pList->m_pEntity));
+			CBaseEntity* pRet = pUnk->GetBaseEntity();
+			if (pRet)
+				return pRet;
+		}
+#else
+		return (CBaseEntity*)pList->m_pEntity;
+#endif
+		pList = pList->m_pNext;
+	}
+
+	return NULL;
+
+}
