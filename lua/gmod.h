a1a46a42fbda7e7d6870717429a16f74b256e276
RaphaelIT7 <raphael.i.t@outlook.de>
2024-12-17 00:09:07 +0100

    luajit: update files again to fix userdata type names being wrong

--- lua/gmod.h
+++ lua/gmod.h
@@ -1,4 +1,14 @@
+#include "lj_obj.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 extern void lua_init_stack_gmod(lua_State* L1, lua_State* L);
 extern void GMOD_LuaPrint(const char* str, lua_State* L);
-extern void* GMOD_LuaCreateEmptyUserdata(lua_State* L);
-extern const char* GMODLUA_GetUserType(lua_State* L, int iType);
+extern void GMOD_LuaCreateEmptyUserdata(lua_State* L);
+
+// Was moved into the lj_api.c file since the linker hated me.
+//extern const char* GMODLUA_GetUserType(lua_State* L, int iType);
+#ifdef __cplusplus
+}
+#endif
