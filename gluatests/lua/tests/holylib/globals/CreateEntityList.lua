ac3c9670086e8b77a64e89395f0c9b0b494c572f
RaphaelIT7 <raphael.i.t@outlook.de>
2025-03-15 14:26:03 +0100

    tests: update them

--- gluatests/lua/tests/holylib/globals/CreateEntityList.lua
+++ gluatests/lua/tests/holylib/globals/CreateEntityList.lua
@@ -1,5 +1,5 @@
 return {
-    groupName = "GetGlobalEntityList",
+    groupName = "CreateEntityList",
     cases = {
         --[[ Function Signature ]]--
         --#region
@@ -7,22 +7,22 @@ return {
             name = "Function exists globally",
             when = IS_BASE_BRANCH,
             func = function()
-                expect( GetGlobalEntityList ).to.beA( "function" )
+                expect( CreateEntityList ).to.beA( "function" )
             end
         },
         {
             name = "Function doesn't exists globally",
             when = IS_64X_BRANCH,
             func = function()
-                expect( GetGlobalEntityList ).to.beA( "nil" )
+                expect( CreateEntityList ).to.beA( "nil" )
             end
         },
         {
             name = "Returns an Table object",
             when = IS_BASE_BRANCH,
             func = function()
-                local entities = GetGlobalEntityList()
-                expect( entities ).to.beA( "table" )
+                local list = CreateEntityList()
+                expect( list ).to.beA( "EntityList" )
             end
         },
     }
