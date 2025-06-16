2dce32fa11c83f627bed9936d3436e296145b974
RaphaelIT7 <raphael.i.t@outlook.de>
2025-06-16 11:48:12 +0200

    bootil: update rapidjson

--- source/bootil/src/Bootil/Data/Json.cpp
+++ source/bootil/src/Bootil/Data/Json.cpp
@@ -2,7 +2,6 @@
 #include "rapidjson/rapidjson.h"
 #include "rapidjson/document.h"
 #include "rapidjson/stringbuffer.h"
-#include "rapidjson/filestream.h"
 #include "rapidjson/prettywriter.h"
 #include "rapidjson/writer.h"
 
