696a10d89d4eef9352c1953ccc4db2cdbc38a8a7
RaphaelIT7 <raphael.i.t@outlook.de>
2025-07-16 11:53:02 +0200

    workflows: test luajit for #55 changes

--- development/module/premake5.lua
+++ development/module/premake5.lua
@@ -77,6 +77,7 @@ CreateWorkspace({name = "holylib", abi_compatible = false})
 			[[../../lua/*.h]],
 			[[../../lua/*.hpp]],
 			[[../../README.md]],
+			[[../../.github/workflows/**.yml]],
 		})
 
 		vpaths({
@@ -84,6 +85,7 @@ CreateWorkspace({name = "holylib", abi_compatible = false})
 			["Lua Headers"] = "../../lua/*.h",
 			["Lua Scrips"] = "../../source/lua/scripts/*.lua",
 			["README"] = "../../README.md",
+			["Workflows"] = "../../.github/workflows/**.yml",
 		})
 
 		removefiles({
