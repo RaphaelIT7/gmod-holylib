bff9379037d49020e0c3527dedaccfb4edf05fbf
Lemon <66844236+FirstLemon@users.noreply.github.com>
2025-10-23 18:47:57 +0200

    Symbols: added FindOrAddGeneric Win Symbols

--- source/symbols.cpp
+++ source/symbols.cpp
@@ -237,8 +237,8 @@ namespace Symbols
 	const std::vector<Symbol> CVEngineServer_PrecacheModelSym = {
 		Symbol::FromName("_ZN14CVEngineServer13PrecacheModelEPKcb"),
 		Symbol::FromSignature("\x55\x48\x89\xE5\x41\x54\x44\x0F\xB6\xE2\x53\x48\x89\xF3\x44\x89\xE6\x48\x89\xDF"), // 55 48 89 E5 41 54 44 0F B6 E2 53 48 89 F3 44 89 E6 48 89
-		Symbol::FromSignature("\x55\x8B\xEC\x56\x8B\x75\x08\x57\x8B\x7D\x0C\x57\x56"), // 55 8B EC 56 8B 75 08 57 8B 7D 0C 57 56 - FUN_10156770
-		Symbol::FromSignature("\x48\x89\x5C\x24\x08\x57\x48\x83\xEC\x20\x48\x8B\xDA\x41\x0F\xB6\xF8"), // 48 89 5c 24 08 57 48 83 EC 20 48 8B DA 41 0F B6 F8 - FUN_180160f40
+		Symbol::FromSignature("\x55\x8B\xEC\x56\x8B\x75\x08\x57\x8B\x7D\x0C\x57\x56"), // 55 8B EC 56 8B 75 08 57 8B 7D 0C 57 56
+		Symbol::FromSignature("\x48\x89\x5C\x24\x08\x57\x48\x83\xEC\x20\x48\x8B\xDA\x41\x0F\xB6\xF8"), // 48 89 5c 24 08 57 48 83 EC 20 48 8B DA 41 0F B6 F8
 	};
 
 	const std::vector<Symbol> CVEngineServer_PrecacheGenericSym = {
@@ -258,6 +258,8 @@ namespace Symbols
 	const std::vector<Symbol> SV_FindOrAddGenericSym = {
 		Symbol::FromName("_Z19SV_FindOrAddGenericPKcb"),
 		Symbol::FromSignature("\x55\x40\x80\xFE\x01\x48\x89\xFE\x48\x89\xE5\x48\x8B\x3D\xC6***\x19\xD2"), // 55 40 80 FE 01 48 89 FE 48 89 E5 48 8B 3D C6 ?? ?? ?? 19 D2
+		Symbol::FromSignature("\x55\x8B\xEC\x51\x8B\x0D\xA0\xBE\x72\x10"), // 55 8B EC 51 8B 0D A0 BE 72 10
+		Symbol::FromSignature("\x48\x89\x5C\x24\x18\x56\x48\x83\xEC\x30\x48\x8B\xF1\x0F\xB6\xDA\x48\x8B\x0D\x89\x63\x71\x00"), // 48 89 5C 24 18 56 48 83 EC 30 48 8B F1 0F B6 DA 48 8B 0D 89 63 71 00
 	};
 
 	//---------------------------------------------------------------------------------
