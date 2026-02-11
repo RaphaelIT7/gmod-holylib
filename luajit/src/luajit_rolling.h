05a22896122960e162a3b38eeaea8cd3333d4b59
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-02-11 17:49:21 +0100

    luajit: mention holylib in the version name

--- luajit/src/luajit_rolling.h
+++ luajit/src/luajit_rolling.h
@@ -30,7 +30,7 @@
 
 #include "lua.h"
 
-#define LUAJIT_VERSION		"LuaJIT 2.1.ROLLING"
+#define LUAJIT_VERSION		"LuaJIT 2.1.ROLLING (HolyLib's Custom Build)"
 #define LUAJIT_VERSION_NUM	20199  /* Deprecated. */
 #define LUAJIT_VERSION_SYM	luaJIT_version_2_1_ROLLING
 #define LUAJIT_COPYRIGHT	"Copyright (C) 2005-2025 Mike Pall"
