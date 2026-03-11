db768a0ff077de9dc7aee771b12071e8794cca7c
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-03-11 04:42:21 +0100

    gmoddatapack: I hate the 64x SDK

--- source/modules/gmoddatapack.cpp
+++ source/modules/gmoddatapack.cpp
@@ -14,6 +14,7 @@
 #include "tier0/memdbgon.h"
 
 #undef isalnum // 64x loves to shit on this one AGAIN
+#undef isalpha // 64x loves to shit on this one AGAIN FUCKING HELL
 
 class CGModDataPackModule : public IModule
 {
