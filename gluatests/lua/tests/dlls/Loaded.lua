49a9f7ffa688f7a03028ad60dd5406a9a70a0c52
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-09-03 10:08:27 +0200

    workflows: use HolyTest
    resolves #85

--- gluatests/lua/tests/dlls/Loaded.lua
+++ gluatests/lua/tests/dlls/Loaded.lua
@@ -11,12 +11,12 @@ return {
                 expect( example.hello() ).to.equal( "Hello World" )
 
                 RunConsoleCommand("holylib_enable_example", "0")
-                HolyLib.ServerExecute() -- If we error, then holylib itself is fked! This function should be exposed on any platform & branch!
+                holytest.ServerExecute() -- If we error, then holylib itself is fked! This function should be exposed on any platform & branch!
 
                 expect( example ).to.beA( "nil" )
 
                 RunConsoleCommand("holylib_enable_example", "1")
-                HolyLib.ServerExecute()
+                holytest.ServerExecute()
 
                 expect( example ).to.beA( "table" )
                 expect( example.hello ).to.beA( "function" )
--- gluatests/lua/tests/modules/filesystem/EasyDirCheck.lua
+++ gluatests/lua/tests/modules/filesystem/EasyDirCheck.lua
@@ -6,19 +6,19 @@ return {
             when = HolyLib_IsModuleEnabled( "HolyLib" ) && HolyLib_IsModuleEnabled( "filesystem" ) and BRANCH ~= "x86-64", -- We depend on HolyLib for convar specific tests. We could switch to use HolyTest / include it which would remove this dependency
             func = function(state)
                 RunConsoleCommand("holylib_filesystem_easydircheck", "0")
-                HolyLib.ServerExecute()
+                holytest.ServerExecute()
 
                 expect(file.IsDir("addons/gluatests/test.folder", "MOD")).to.beTrue()
                 expect(file.IsDir("addons/gluatests/test.folder/folder2", "MOD")).to.beTrue()
 
                 RunConsoleCommand("holylib_filesystem_easydircheck", "1")
-                HolyLib.ServerExecute()
+                holytest.ServerExecute()
 
                 expect(file.IsDir("addons/gluatests/test.folder", "MOD")).to.beFalse() -- It's expected behavior for this to fail.
                 expect(file.IsDir("addons/gluatests/test.folder/folder2", "MOD")).to.beTrue()
 
                 RunConsoleCommand("holylib_filesystem_easydircheck", GetConVar("holylib_filesystem_easydircheck"):GetDefault()) -- Restoring to default
-                HolyLib.ServerExecute()
+                holytest.ServerExecute()
             end,
         },
     }
