14fee90ececa18e0a558544b56330fd4548d81a1
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-09-05 19:31:01 +0200

    filesystem: more debugging

--- source/modules/filesystem.cpp
+++ source/modules/filesystem.cpp
@@ -633,7 +633,7 @@ static bool hook_CBaseFileSystem_IsDirectory(CBaseFileSystem* _this, const char*
 #ifdef SUPPORT_PACKED_STORE
 		if ( pSearchPath->GetPackedStore() )
 		{
-			Msg("What the heck (%s)\n", pFileName);
+			Msg("What the heck (%s - %s - %p)\n", pFileName, pSearchPath->GetPathString(), pSearchPath->GetPackedStore());
 			// GMod
 			if ( func_CPackedStore_DirectoryEntryExists( pSearchPath->GetPackedStore(), pFileName ) )
 				return true;
