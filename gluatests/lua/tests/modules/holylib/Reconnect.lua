d888b2cda269f04cf3b4e90816bb959dbc2249c7
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-09-22 11:57:42 +0200

    tests: update error message

--- gluatests/lua/tests/modules/holylib/Reconnect.lua
+++ gluatests/lua/tests/modules/holylib/Reconnect.lua
@@ -28,7 +28,7 @@ return {
             func = function()
                 local ent = MakeTestEntity()
 
-                expect( HolyLib.Reconnect, ent ).to.errWith( "bad argument #1 to '?' (Player entity is NULL or not a player (!?))" )
+                expect( HolyLib.Reconnect, ent ).to.errWith( "bad argument #1 to '?' (Failed to get a valid CBaseClient!)" )
 
                 SafeRemoveEntity( ent )
             end
